59dd9c262b34456942956e787516189c2eef5ce5
Adriana Disano <adrianadisano@gmail.com>
2019-10-11 09:48:32 -0500

    styled and polished UI

--- base.js
+++ base.js
@@ -122,17 +122,25 @@ var importText = function(){
 	}
 }
 
+//accordion animations animated-panel
+
+
+var acc = document.getElementsByClassName("accordion");
+var a;
+
+for (a = 0; a < acc.length; a++) {
+  acc[a].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
 $(document).ready(function(){
-//allow users to open and close the "custom unit of time" selection menu
-	$("#timeUnitButton").click(function(){
-		$(".timeUnitContainer").toggle();
-	}
-);
-//allows uers to open and close the "add event" menu
-	$("#addEventButton").click(function(){
-		$(".addEventContainer").toggle();
-	}
-);
 	$("#eventTimeInput").val($("#eventTimeInput").val() +
 		$("#timeUnitInput").val() + "s?");
 });
